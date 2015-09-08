75a925467685bba34225d494911e7a9e45683951
pengs <martinp.shao@gmail.com>
2015-09-07 22:27:32 -0500

    22:27 PM SEPT 7 2015

--- test.R
+++ test.R
@@ -11,13 +11,23 @@ ConfusionTable <- function(ct){
                        "Total Error Rate")
     return(result)
 }
-cor(Boston[, 1], Boston[, -1])
-subset <- c(FALSE, abs(cor(Boston[, 1], Boston[, -1])) > 0.3, TRUE)
+subset1 <- c(FALSE, abs(cor(Boston[, 1], Boston[, -1])) > 0.3, TRUE)
+subset2 <- c(FALSE, abs(cor(Boston[, 1], Boston[, -1])) > 0.4, TRUE)
+subset3 <- c(FALSE, abs(cor(Boston[, 1], Boston[, -1])) > 0.5, TRUE)
 Boston$crim01 <- as.numeric(Boston$crim > median(Boston$crim))
 set.seed(1)
 rands <- runif(nrow(Boston))
 test <- rands > quantile(rands, 0.75)
 train <- !test
-Boston.train <- Boston[train, subset]
-Boston.test <- Boston[test, subset]
-Boston.train$crim01 <- factor(Boston.train$crim01)
+Boston.train <- Boston[train, -1]
+Boston.test <- Boston[test, -1]
+glm.fit=glm(crim01 ~ ., data=Boston.train, family = binomial)
+summary(glm.fit)
+
+
+
+lda.fit <- lda(crim01 ~ nox + rad + ptratio, data=Boston.train)
+lda.pred=predict(lda.fit,Boston.test)$class
+ct <- table(lda.pred,Boston.test$crim01)
+ct
+ConfusionTable(ct)
