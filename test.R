520d210a5844935fe0ff75c88a8482464f7da451
pengs <martinp.shao@gmail.com>
2015-09-07 20:11:55 -0500

    20:11 PM SEPT 7 2015

--- test.R
+++ test.R
@@ -1,9 +1,23 @@
-#code test
-#4.10
 rm(list = ls())
-library(ISLR)
-library(ggplot2)
-attach(Weekly)
-summary(Weekly)
-pairs(Weekly)
-cor(Weekly[, -9])
+ConfusionTable <- function(ct){
+    accuracy <- (ct[1, 1] + ct[2, 2]) / (sum(ct))
+    TPr <- ct[2, 2] / (ct[1, 2] + ct[2, 2])
+    FPr <- ct[2, 1] / (ct[1, 1] + ct[2, 1])
+    precision <- ct[2, 2] / (ct[2, 1] + ct[2, 2])
+    error <- 1 - accuracy
+    result <- list(accuracy, TPr, FPr, precision, error)
+    names(result) <- c("Accuracy", "True Positive Rate", 
+                       "False Posistive Rate", "Precision", 
+                       "Total Error Rate")
+    return(result)
+}
+cor(Boston[, 1], Boston[, -1])
+subset <- c(FALSE, abs(cor(Boston[, 1], Boston[, -1])) > 0.3, TRUE)
+Boston$crim01 <- as.numeric(Boston$crim > median(Boston$crim))
+set.seed(1)
+rands <- runif(nrow(Boston))
+test <- rands > quantile(rands, 0.75)
+train <- !test
+Boston.train <- Boston[train, subset]
+Boston.test <- Boston[test, subset]
+Boston.train$crim01 <- factor(Boston.train$crim01)
